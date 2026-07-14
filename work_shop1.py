33c778c1d0ee814a98a0537ae92de887ba8be547
Nebula <181827476+Nebula-NMV@users.noreply.github.com>
2026-07-14 16:51:12 +0700

    ยังไม่เสร็จจ้าา

---
+++ work_shop1.py
@@ -0,0 +1,16 @@
+quantity = int(input())
+cost_price = int(input())
+sell_price = int(input())
+team_members = int( input())
+
+cost = quantity * cost_price
+revenue = sell_price * quantity
+profit = revenue - cost
+boss =  profit % 20
+henchman = 
+
+print("ต้นทุนทั้งหมด (บาท)")
+print("รายรับทั้งหมด (บาท)")
+print("กำไรสุทธิ (บาท)")
+print("จำนวนเงินที่หักไปให้บอส (บาท)")
+print("จำนวนเงินที่ลูกน้องแต่ละคนได้ (บาท)")
---
+++ work_shop2.py
@@ -0,0 +1,9 @@
+name = input("ชื่อ :")
+age = input("อายุ :")
+tail = input("ส่วนสูง :")
+stg = int(input("พลัง :"))
+money = int(input("เงินติดตัว :"))
+
+if name == "Nebula" or age >= 15 and age >= 60 and tail >= 160: 
+    print("รับเข้าทำงาน")
+
