7b975a2f55c4ff4457946edd182120e8d60aafc5
Nebula [NMV] <nebula.nmv@gmail.com>
2026-07-14 21:28:19 +0700

    เสร็จมั้งนะ

--- work_shop1.py
+++ work_shop1.py
@@ -1,16 +1,19 @@
-quantity = int(input())
-cost_price = int(input())
-sell_price = int(input())
-team_members = int( input())
+print("---------------------------------")
+quantity = int(input("จำนวนปืนที่รับมาขาย : "))
+cost_price = int(input("ต้นทุนของปืนที่รับมา : "))
+sell_price = int(input("ราคาที่จะนำไปขายต่อ : "))
+team_members = int( input("จำนวนลูกน้องในทีมที่ไปทำงาน : "))
 
 cost = quantity * cost_price
 revenue = sell_price * quantity
 profit = revenue - cost
-boss =  profit % 20
-henchman = 
+boss =  profit * 0.20
+pay_member = (profit - boss) / team_members
 
-print("ต้นทุนทั้งหมด (บาท)")
-print("รายรับทั้งหมด (บาท)")
-print("กำไรสุทธิ (บาท)")
-print("จำนวนเงินที่หักไปให้บอส (บาท)")
-print("จำนวนเงินที่ลูกน้องแต่ละคนได้ (บาท)")
+print("---------------------------------")
+print(f"ต้นทุนทั้งหมด {cost} (บาท)")
+print(f"รายรับทั้งหมด {revenue} (บาท)")
+print(f"กำไรสุทธิ {profit} (บาท)")
+print(f"จำนวนเงินที่หักไปให้บอส {boss} (บาท)")
+print(f"จำนวนเงินที่ลูกน้องแต่ละคนได้ {pay_member} (บาท)")
+print("---------------------------------")
